2b41f4c9599925e7f572277dd35638ad3a903ffe
Hugo <hugo.rock20@hotmail.com>
2018-06-29 00:44:26 -0500

    create _LayoutLogin.cshtml for login, now home has its own layout

---
+++ APPRestaurante/APPRestaurante.Web/Areas/Admin/Controllers/InicioController.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace APPRestaurante.Web.Areas.Admin.Controllers
+{
+    public class InicioController : Controller
+    {
+        // GET: Admin/Inicio
+        public ActionResult Index()
+        {
+            return View();
+        }
+    }
+}
