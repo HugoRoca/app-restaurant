f7624237a82d8f934fd7a015dd5147ff3934a39f
Hugo <hugo.rock20@hotmail.com>
2018-06-30 23:39:49 -0500

    prepare project for helper

---
+++ APPRestaurante/APPRestaurante.Helper/SessionHelper.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Web;
+using System.Web.Security;
+
+namespace APPRestaurante.Helper
+{
+    public class SessionHelper
+    {
+        // more information http://anexsoft.com/p/91/clase-para-agilizar-la-autenticacion-en-asp-net-mvc
+        public static bool ExistUserInSession()
+        {
+            return HttpContext.Current.User.Identity.IsAuthenticated;
+        }
+
+        public static void DestroyUserSession()
+        {
+            FormsAuthentication.SignOut();
+        }
+
+        public static int GetUser()
+        {
+            int user_id = 0;
+            if (HttpContext.Current.User != null && HttpContext.Current.User.Identity is FormsIdentity)
+            {
+                FormsAuthenticationTicket ticket = ((FormsIdentity)HttpContext.Current.User.Identity).Ticket;
+                if (ticket != null)
+                {
+                    user_id = Convert.ToInt32(ticket.UserData);
+                }
+            }
+            return user_id;
+        }
+
+        public static void AddUserToSession(string id)
+        {
+            bool persist = true;
+            var cookie = FormsAuthentication.GetAuthCookie("usuario", persist);
+
+            cookie.Name = FormsAuthentication.FormsCookieName;
+            cookie.Expires = DateTime.Now.AddMonths(3);
+
+            var ticket = FormsAuthentication.Decrypt(cookie.Value);
+            var newTicket = new FormsAuthenticationTicket(ticket.Version, ticket.Name, ticket.IssueDate, ticket.Expiration, ticket.IsPersistent, id);
+
+            cookie.Value = FormsAuthentication.Encrypt(newTicket);
+            HttpContext.Current.Response.Cookies.Add(cookie);
+        }
+    }
+}
