ad2743c34fa1c098a4bb6abb8828c05b650a8a51
Hugo <hugo.rock20@hotmail.com>
2018-07-03 23:42:11 -0500

    BundleConfig create

--- APPRestaurante/APPRestaurante.Web/App_Start/BundleConfig.cs
+++ APPRestaurante/APPRestaurante.Web/App_Start/BundleConfig.cs
@@ -7,11 +7,11 @@ namespace APPRestaurante.Web.App_Start
         public static void RegisterBundles(BundleCollection bundles)
         {
             bundles.Add(
-                new StyleBundle("~/bundles/cssLogin")
-                .Include(""));
+                new ScriptBundle("~/bundles/usuario")
+                .Include("~/Scritps/Usuario/index.js"));
 
             bundles.Add(new DynamicFolderBundle("js", "*.js", false, new JsMinify()));
-            bundles.Add(new DynamicFolderBundle("css","*.css", false, new CssMinify()));
+            //bundles.Add(new DynamicFolderBundle("css","*.css", false, new CssMinify()));
 
 #if DEBUG
             BundleTable.EnableOptimizations = false;
--- APPRestaurante/APPRestaurante.Web/Global.asax.cs
+++ APPRestaurante/APPRestaurante.Web/Global.asax.cs
@@ -1,10 +1,6 @@
 ﻿using APPRestaurante.Web.App_Start;
 using LightInject;
-using System;
-using System.Collections.Generic;
-using System.Linq;
 using System.Reflection;
-using System.Web;
 using System.Web.Mvc;
 using System.Web.Optimization;
 using System.Web.Routing;
