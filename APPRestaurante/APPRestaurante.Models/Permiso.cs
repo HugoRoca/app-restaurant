0e6ce2c65d0b709b69a5c209b3cb4bb3f3ab1c3a
Hugo <hugo.rock20@hotmail.com>
2018-07-08 23:17:36 -0500

    create class Permiso.cs and PermisoUsuario.cs

---
+++ APPRestaurante/APPRestaurante.Models/Permiso.cs
@@ -0,0 +1,14 @@
+﻿using Dapper.Contrib.Extensions;
+
+namespace APPRestaurante.Models
+{
+    [Table("Permiso")]
+    public class Permiso
+    {
+        public int id { get; set; }
+        public string descripcion { get; set; }
+        public bool estado { get; set; }
+        public int padre { get; set; }
+        public string url { get; set; }
+    }
+}
---
+++ APPRestaurante/APPRestaurante.Models/PermisoUsuario.cs
@@ -0,0 +1,13 @@
+﻿using Dapper.Contrib.Extensions;
+
+namespace APPRestaurante.Models
+{
+    [Table("PermisoUsuario")]
+    public class PermisoUsuario
+    {
+        public int id { get; set; }
+        public int idUsuario { get; set; }
+        public int idPermiso { get; set; }
+        public bool estado { get; set; }
+    }
+}
