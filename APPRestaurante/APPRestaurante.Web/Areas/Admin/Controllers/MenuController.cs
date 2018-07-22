7b03a9b5008950a7bbcad994bf0689f679ef7ee4
Hugo <hugo.rock20@hotmail.com>
2018-07-21 23:59:22 -0500

    commit

--- APPRestaurante/APPRestaurante.Web/Areas/Admin/Controllers/MenuController.cs
+++ APPRestaurante/APPRestaurante.Web/Areas/Admin/Controllers/MenuController.cs
@@ -21,7 +21,12 @@ namespace APPRestaurante.Web.Areas.Admin.Controllers
             return View();
         }
 
-        [Route("Count/{rows:int}")]
+        public ActionResult Registro()
+        {
+            return View();
+        }
+
+        [HttpPost]
         public JsonResult Count(int rows)
         {
             var total = _unit.Menu.Count();
@@ -31,7 +36,15 @@ namespace APPRestaurante.Web.Areas.Admin.Controllers
                 TotalPages = totalPagina
             };
 
-            return Json(page);
+            return Json(new { Page = page});
+        }
+
+        [Route("ListaMenu/{page:int}/{row:int}")]
+        public PartialViewResult ListaMenu(int page, int row)
+        {
+            var start = ((page - 1) * row) + 1;
+            var end = page * row;
+            return PartialView(_unit.Menu.ListaMenuPaginacion(DateTime.Now, DateTime.Now, start, end));
         }
     }
 }
