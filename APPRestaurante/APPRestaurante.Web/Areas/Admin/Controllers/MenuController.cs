5209a0c0a3519ee93700564c4be692755da23a8a
Hugo <hugo.rock20@hotmail.com>
2018-07-22 22:16:10 -0500

    jquery.bootpag add

--- APPRestaurante/APPRestaurante.Web/Areas/Admin/Controllers/MenuController.cs
+++ APPRestaurante/APPRestaurante.Web/Areas/Admin/Controllers/MenuController.cs
@@ -40,12 +40,11 @@ namespace APPRestaurante.Web.Areas.Admin.Controllers
             return Json(new { Page = page});
         }
 
-        [Route("ListaMenu/{page:int}/{row:int}")]
-        public PartialViewResult ListaMenu(int page, int row)
+        public JsonResult ListaMenu(int page, int row)
         {
             var start = ((page - 1) * row) + 1;
             var end = page * row;
-            return PartialView(_unit.Menu.ListaMenuPaginacion(DateTime.Now, DateTime.Now, start, end));
+            return Json(_unit.Menu.ListaMenuPaginacion(DateTime.Now, DateTime.Now, start, end));
         }
     }
 }
