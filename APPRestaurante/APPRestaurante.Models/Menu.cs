51ee1485871c7ee47fe615321087eb74f69fb332
HugoRoca <hugo.rock20@hotmail.com>
2018-07-27 16:34:01 -0500

    Delete [require]

--- APPRestaurante/APPRestaurante.Models/Menu.cs
+++ APPRestaurante/APPRestaurante.Models/Menu.cs
@@ -5,20 +5,13 @@ namespace APPRestaurante.Models
     public class Menu
     {
         public int id { get; set; }
-        [Required]
         public string fecha { get; set; }
         public string usuario { get; set; }
-        [Required]
         public string titulo { get; set; }
-        [Required]
         public string descripcion { get; set; }
-        [Required]
         public string tipo { get; set; }
-        [Required]
         public double precio { get; set; }
-        [Required]
         public string foto { get; set; }
-        [Required]
         public int idUsuario { get; set; }
     }
 }
