9641ac7e117c8670dcd7890cbf6026cbe99f8069
Hugo <hugo.rock20@hotmail.com>
2018-07-30 23:12:42 -0500

    config bundles for login

--- APPRestaurante/APPRestaurante.Web/App_Start/BundleConfig.cs
+++ APPRestaurante/APPRestaurante.Web/App_Start/BundleConfig.cs
@@ -7,8 +7,16 @@ namespace APPRestaurante.Web.App_Start
         public static void RegisterBundles(BundleCollection bundles)
         {
             bundles.Add(
-                new ScriptBundle("~/bundles/usuario")
-                .Include("~/Scritps/Usuario/index.js"));
+                new StyleBundle("~/bundles/CSS-Usuario")
+                .Include("~/Areas/Admin/Assets/css/bootstrap.css")
+                .Include("~/Areas/Admin/Assets/css/font-awesome.css")
+                .Include("~/Areas/Admin/Assets/css/ace-fonts.css")
+                .Include("~/Areas/Admin/Assets/css/ace.css"));
+
+            bundles.Add(
+                new ScriptBundle("~/bundles/JS-Usuario")
+                .Include("~/Areas/Admin/Assets/js/jquery.js")
+                .Include("~/Areas/Admin/Assets/js/jquery.mobile.custom.js"));
 
             bundles.Add(new DynamicFolderBundle("js", "*.js", false, new JsMinify()));
             //bundles.Add(new DynamicFolderBundle("css","*.css", false, new CssMinify()));
