7caccb7cb4b8a97a582bae2c2503fde6a8651f93
HugoRoca <hugo.rock20@hotmail.com>
2018-08-06 15:53:08 -0500

    add controller and view user

---
+++ APPRestaurante/APPRestaurante.Web/Areas/Admin/Controllers/UsuarioController.cs
@@ -0,0 +1,25 @@
+﻿using APPRestaurante.UnitOfWork;
+using APPRestaurante.Web.Areas.Admin.Filters;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace APPRestaurante.Web.Areas.Admin.Controllers
+{
+    [Autenticado]
+    public class UsuarioController : BaseController
+    {
+        public UsuarioController(IUnitOfWork unit) : base(unit)
+        {
+
+        }
+
+        // GET: Admin/Usuario
+        public ActionResult Index()
+        {
+            return View();
+        }
+    }
+}
