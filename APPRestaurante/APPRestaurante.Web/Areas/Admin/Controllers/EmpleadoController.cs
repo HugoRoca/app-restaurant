eee969181c72860b783c2257d216e16e6227ce90
HugoRoca <hugo.rock20@hotmail.com>
2018-08-06 12:04:57 -0500

    add controller and view Empleado

---
+++ APPRestaurante/APPRestaurante.Web/Areas/Admin/Controllers/EmpleadoController.cs
@@ -0,0 +1,23 @@
+﻿using APPRestaurante.UnitOfWork;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace APPRestaurante.Web.Areas.Admin.Controllers
+{
+    public class EmpleadoController : BaseController
+    {
+        public EmpleadoController(IUnitOfWork unit) : base(unit)
+        {
+
+        }
+
+        // GET: Admin/Empleado
+        public ActionResult Index()
+        {
+            return View();
+        }
+    }
+}
