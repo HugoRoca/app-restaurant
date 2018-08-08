a0d82d3f8a059d6ed88ad6bd93dab88c5de64ed3
HugoRoca <hugo.rock20@hotmail.com>
2018-08-08 12:02:51 -0500

    add new controller/cobranza and view Index.cshtml

---
+++ APPRestaurante/APPRestaurante.Web/Areas/Admin/Controllers/CobranzaController.cs
@@ -0,0 +1,24 @@
+﻿using APPRestaurante.UnitOfWork;
+using APPRestaurante.Web.Areas.Admin.Filters;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace APPRestaurante.Web.Areas.Admin.Controllers
+{
+    [Autenticado]
+    public class CobranzaController : BaseController
+    {
+        public CobranzaController(IUnitOfWork unit) : base(unit)
+        {
+        }
+
+        // GET: Admin/Cobranza
+        public ActionResult Index()
+        {
+            return View();
+        }
+    }
+}
