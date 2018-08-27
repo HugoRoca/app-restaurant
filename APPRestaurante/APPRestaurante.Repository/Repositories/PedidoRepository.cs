7a7abd8cc3a1096ae332bb7de8c8871334873b3f
Hugo <hugo.rock20@hotmail.com>
2018-08-26 22:54:07 -0500

    finished method for insert pedido

--- APPRestaurante/APPRestaurante.Repository/Repositories/PedidoRepository.cs
+++ APPRestaurante/APPRestaurante.Repository/Repositories/PedidoRepository.cs
@@ -1,10 +1,9 @@
 ﻿using APPRestaurante.Models;
 using APPRestaurante.Repository.Interfaces;
+using Dapper.Contrib.Extensions;
 using System;
 using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+using System.Data.SqlClient;
 
 namespace APPRestaurante.Repository.Repositories
 {
@@ -12,7 +11,30 @@ namespace APPRestaurante.Repository.Repositories
     {
         public bool PedidoyDetallePedido(Pedido pedido, IEnumerable<PedidoDetalle> items)
         {
-            throw new NotImplementedException();
+            using (var connection = new SqlConnection(_connectionString))
+            {
+                connection.Open();
+                using (var transaction = connection.BeginTransaction())
+                {
+                    try
+                    {
+                        var id = (int)connection.Insert(pedido, transaction);
+                        foreach (var pedidoItem in items)
+                        {
+                            pedidoItem.idPedido = id;
+                            connection.Insert(pedidoItem, transaction);
+                        }
+                        transaction.Commit();
+                        return true;
+
+                    }
+                    catch (Exception)
+                    {
+                        transaction.Rollback();
+                        throw;
+                    }
+                }
+            }
         }
     }
 }
