da676ecb3b710ae1f3a104014b1755550a015995
Hugo <hugo.rock20@hotmail.com>
2018-09-02 18:17:01 -0500

    select result

---
+++ APPRestaurante/APPRestaurante.Web/Areas/Admin/Models/CobranzaModel.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace APPRestaurante.Web.Areas.Admin.Models
+{
+    public class CobranzaModel
+    {
+        public int id { get; set; }
+        public DateTime fecha { get; set; }
+        public double total { get; set; }
+        public string totalString { get; set; }
+        public string nombres { get; set; }
+        public int mesa { get; set; }
+        public string tipoPago { get; set; }
+        public int idEmpleado { get; set; }
+        public int idUsuario { get; set; }
+        public int estado { get; set; }
+        public IEnumerable<PedidoDetalleModel> pedidoDetalles { get; set; }
+    }
+}
--- APPRestaurante/APPRestaurante.Web/Scripts/Cobro/index.js
+++ APPRestaurante/APPRestaurante.Web/Scripts/Cobro/index.js
@@ -1,1 +1,32 @@
-﻿
+﻿var cobroModule = (function (globalData, $) {
+    'use strict';
+
+    var me = {};
+
+    var urls = globalData.urlProvider;
+
+    me.Servicios = (function () {
+
+    })();
+
+    me.Eventos = (function () {
+
+    })();
+
+    me.Funciones = (function () {
+
+    })();
+
+    me.Inicializar = function () {
+        me.Funciones.inicializarEventos();
+    }
+
+    return me;
+
+})(cobroData, jQuery);
+
+window.cobroModule = cobroModule;
+
+$(document).ready(function () {
+    cobroModule.Inicializar();
+});
